6a370f7c09e6333b20c789c1e6f4c0cfd2060385
KygekDev <68368066+KygekDev@users.noreply.github.com>
2021-12-05 08:50:26 +0800

    Implement #3; untested because coded on smartphone

--- src/KygekTeam/KtpmplCfs/KtpmplCfs.php
+++ src/KygekTeam/KtpmplCfs/KtpmplCfs.php
@@ -20,32 +20,52 @@ use pocketmine\plugin\Plugin;
 class KtpmplCfs {
 
     /**
-     * Checks for configuration file version.
-     * If doesn't match the provided version, will send message to Logger, rename existing configuration file and generate new configuration file.
-     *
+     * KtpmplCfs constructor.
+     * 
      * @param Plugin $plugin
-     * @param string $version
      */
-    public static function checkConfig(Plugin $plugin, string $version) {
+    public function __construct(
+        private Plugin $plugin
+    ) {}
+
+    /**
+     * Checks for configuration file version.
+     * 
+     * If doesn't match the provided version, will send message to Logger, rename existing configuration file and generate new configuration file.
+     * 
+     * Returns true if configuration file version matches the provided version, otherwise false.
+     *
+     * @param string $version
+     * @return bool
+     */
+    public function checkConfig(string $version) : bool {
+        $plugin = $this->plugin;
         if ($plugin->getConfig()->get("config-version") !== $version) {
             $plugin->getLogger()->notice("Your configuration file is outdated, updating the config.yml...");
             $plugin->getLogger()->notice("The old configuration file can be found at config_old.yml");
             rename($plugin->getDataFolder()."config.yml", $plugin->getDataFolder()."config_old.yml");
             $plugin->saveDefaultConfig();
             $plugin->getConfig()->reload();
+            return false;
         }
+        return true;
     }
 
     /**
      * Checks for plugin updates in Poggit.
      * Plugin must include the UpdateNotifier virion.
      *
-     * @param Plugin $plugin
+     * Returns true if update check is enabled in config.yml, otherwise false.
+     *
+     * @return bool
      */
-    public static function checkUpdates(Plugin $plugin) {
+    public function checkUpdates() : bool {
+        $plugin = $this->plugin;
         if ($plugin->getConfig()->get("check-updates", true)) {
             UpdateNotifier::checkUpdate($plugin->getDescription()->getName(), $plugin->getDescription()->getVersion());
+            return true;
         }
+        return false;
     }
 
 }
