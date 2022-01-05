ff1ba911c56b58b8cc0eb0f9604e1ad3fad017f4
KygekDev <68368066+KygekDev@users.noreply.github.com>
2022-01-05 23:20:48 +0800

    Closes #3; Merge branch `4.0.0-dev` to `main` (#6)
    * Bump API and virion version
    
    * Add pm4 branch and remove UpdateNotifier library
    
    * Require PHP 7.4 or newer
    
    * Now implemented UpdateNotifier inside virion
    
    * Core is case-sensitive
    
    * Require PHP version of ^8.0
    
    Also changed from `extensions: Core:` to `php:`
    
    * [ci skip] Idk what to name this commit
    
    * Bump version
    
    * Build from this branch and using UpdateNotifier again
    
    * Using UpdateNotifier again
    
    * Using UpdateNotifier branch API-4.0.0
    
    * Bump virion version to 4.0.0-BETA and add branch in .poggit.yml
    
    * Implement #3; untested because coded on smartphone
    
    * Change Plugin to PluginBase, update PHPDoc and implement #2
    
    Closes #2
    
    * Add .gitignore
    
    * Add composer.json
    
    * Revert "Add .gitignore"
    
    This reverts commit 18a20e34b6e7e5898d1ce79e28c574f889519e7f.
    
    * Revert "Add composer.json"
    
    This reverts commit 5ee231f4d9e4d395e036a10c81a8ebc263b60b67.
    
    * Many changes
    
    - Update copyright year
    - Add "Development version check" feature in README.md
    - Improve PHPDoc
    - Add $onlyCheck option to all methods
    
    * Add an option to use different key in the config in all methods
    
    * Add plugin for testing the virion
    
    * [ci skip] Add plugin installation instructions
    
    * Fix command not working in the console
    
    * [ci skip] Prepare version 4.0.0 release

--- src/KygekTeam/KtpmplCfs/KtpmplCfs.php
+++ src/KygekTeam/KtpmplCfs/KtpmplCfs.php
@@ -2,7 +2,7 @@
 
 /*
  * Virion to easily implement common functionalities on KygekTeam PocketMine-MP plugins (e.g. config version check)
- * Copyright (C) 2021 KygekTeam
+ * Copyright (C) 2021-2022 KygekTeam
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,37 +15,79 @@ declare(strict_types=1);
 namespace KygekTeam\KtpmplCfs;
 
 use JackMD\UpdateNotifier\UpdateNotifier;
-use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
 
 class KtpmplCfs {
 
     /**
+     * KtpmplCfs constructor.
+     * 
+     * @param PluginBase $plugin
+     */
+    public function __construct(
+        private PluginBase $plugin
+    ) {}
+
+    /**
      * Checks for configuration file version.
+     *
      * If doesn't match the provided version, will send message to Logger, rename existing configuration file and generate new configuration file.
      *
-     * @param Plugin $plugin
-     * @param string $version
+     * @param string $version   The configuration file version to check.
+     * @param string $key   Configuration file key to check, default is config-version.
+     * @param bool $onlyCheck   Whether to only check the configuration file version, default is false.
+     * @return bool     True if configuration file version matches the provided version, otherwise false.
      */
-    public static function checkConfig(Plugin $plugin, string $version) {
-        if ($plugin->getConfig()->get("config-version") !== $version) {
-            $plugin->getLogger()->notice("Your configuration file is outdated, updating the config.yml...");
-            $plugin->getLogger()->notice("The old configuration file can be found at config_old.yml");
-            rename($plugin->getDataFolder()."config.yml", $plugin->getDataFolder()."config_old.yml");
-            $plugin->saveDefaultConfig();
-            $plugin->getConfig()->reload();
+    public function checkConfig(string $version, string $key = "config-version", bool $onlyCheck = false) : bool {
+        $plugin = $this->plugin;
+        if ($plugin->getConfig()->get($key) !== $version) {
+            if (!$onlyCheck) {
+                $plugin->getLogger()->notice("Your configuration file is outdated, updating the config.yml...");
+                $plugin->getLogger()->notice("The old configuration file can be found at config_old.yml");
+                rename($plugin->getDataFolder() . "config.yml", $plugin->getDataFolder() . "config_old.yml");
+                $plugin->saveDefaultConfig();
+                $plugin->getConfig()->reload();
+            }
+            return false;
         }
+        return true;
     }
 
     /**
-     * Checks for plugin updates in Poggit.
-     * Plugin must include the UpdateNotifier virion.
+     * Checks for plugin updates in Poggit using the UpdateNotifier virion.
      *
-     * @param Plugin $plugin
+     * @param string $key   Configuration file key to check, default is check-updates.
+     * @param bool $onlycheck   Whether to only check if plugin updates checking is enabled in configuration file, default is false.
+     * @return bool     True if plugin updates checking is enabled in configuration file, otherwise false.
      */
-    public static function checkUpdates(Plugin $plugin) {
-        if ($plugin->getConfig()->get("check-updates", true)) {
-            UpdateNotifier::checkUpdate($plugin->getDescription()->getName(), $plugin->getDescription()->getVersion());
+    public function checkUpdates(string $key = "check-updates", bool $onlycheck = false) : bool {
+        $plugin = $this->plugin;
+        if ($plugin->getConfig()->get($key, true)) {
+            if (!$onlycheck) {
+                UpdateNotifier::checkUpdate($plugin->getDescription()->getName(), $plugin->getDescription()->getVersion());
+            }
+            return true;
         }
+        return false;
+    }
+
+    /**
+     * Send a warning to the console that the plugin is running on a development version.
+     *
+     * @param string $key   Configuration file key to check, default is warn-development.
+     * @param bool $onlyCheck   Whether to only check if arn development version is enabled in configuration file, default is false.
+     * @return bool     True if warn development version is enabled in configuration file, otherwise false.
+     */
+    public function warnDevelopmentVersion(string $key = "warn-development", bool $onlyCheck = false) : bool {
+        $plugin = $this->plugin;
+        if ($plugin->getConfig()->get($key, true)) {
+            if (!$onlyCheck) {
+                $name = $plugin->getName();
+                $plugin->getLogger()->warning("This plugin is running on a development version. There might be some major bugs. If you found one, please submit an issue in https://github.com/KygekTeam/$name/issues.");
+            }
+            return true;
+        }
+        return false;
     }
 
 }
